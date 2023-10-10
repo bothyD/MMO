a97c231f713e38ffc0d188b46c1afa6948efbaaf
bothyD <pavla.acc@mail.ru>
2023-10-10 12:48:48 +0700

    edit lab2

--- lab2/main.py
+++ lab2/main.py
@@ -1,39 +1,56 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn import tree
 
-def makeTestTrainMas(df, longS, longAll):
-    X_test = df['MrotInHour'][0:longS].values
-    Y_test = df['Salary'][0:longS].values
-    class_test = df['Class'][0:longS].values
-    
+def impute(dataset):
+    imputer = SimpleImputer(missing_values=np.NaN, strategy='mean')
+    imputer.fit(dataset)
+    return imputer.transform(dataset)
 
-    X_train = df['MrotInHour'][longS:longAll].values
-    Y_train = df['Salary'][longS:longAll].values
-    class_train = df['Class'][longS:longAll].values
-    
-
-    XY_test = np.vstack((X_test, Y_test,class_test))
-    XY_train = np.vstack((X_train, Y_train,class_train))
-
-    return XY_test, XY_train
-
-X = np.array([[1, 2], [3, 4], [5, 6]])
-y = np.array([0, 1, 0])
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
-#???????????????????????????????simpleInputer
 
 def main():
-    df = pd.read_csv('heart_data.csv')
-    longAll = len(df['age'])
-    longS = int(longAll/3)
-    XY_test, XY_train = makeTestTrainMas(df, longS, longAll)
+    data = pd.read_csv('heart_data.csv')
+    data = data.replace('?', np.NaN)
+    print(data)
 
-    X = np.array([[1, 2], [3, 4], [5, 6]])
-    y = np.array([0, 1, 0])
-    clf = DecisionTreeClassifier()
-    clf.fit(X, y)
+    predictors = ['age','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']
+    outcome = ['goal']
+    xx = data.loc[1:, predictors]
+    yy = data.loc[1:, outcome]
+    X = impute(xx)
+    y = impute(yy)
 
+    # разбиение на обучающую и тестовую выборки
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    best_acc=0
+    for i in range(15,25,2):
+
+        clf = DecisionTreeClassifier(max_depth=i-5, max_leaf_nodes=i)
+        clf.fit(X_train,y_train)
+        
+        prediction = clf.predict(X_train)
+        acc = accuracy_score(y_train, prediction)
+        print("deph = ", i-5, "leaves = ",i,", Accuracy: {:.4%}".format(acc))
+        if best_acc<acc:
+            best_acc=acc
+            best_index= i
+            best_deph = i-5
+    print("depth = ",best_deph,", n_leaves = ",best_index,", Best Accuracy: {:.4%}".format(best_acc))
+    # plt.figure(figsize=(12,8))
+    # tree.plot_tree(clf, rounded=True, filled=True, feature_names=prediction)
+    # plt.show()
+    # plt.savefig('tree.png')
+    # или
+    # text_rep = tree.export_text(clf)
+    # print(text_rep)
+
+    # print("depth: ", clf.get_depth())
+    # print("Leaves: ", clf.get_n_leaves())
+    
 if __name__ =="__main__":
     main()
